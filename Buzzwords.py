b5b2b8d0e1c81ac22be3f672b2b92ec37ee86212
Saida <covrksaida@gmail.com>
2024-04-19 23:02:57 +0200

    Grid Anpassungen
    Habe die Grid angepasst, jedoch ist in Zeile 37 noch ein Bug, welcher mir bewusst ist
    Bewegung innerhalb der Grid fehlt noch
    Zudem habe ich versucht etwas "aufzuräumen"
    "Methoden" fehlen noch, um ordentlicher zu sein

--- Buzzwords.py
+++ Buzzwords.py
@@ -1,36 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+import random
 #import textual
 spielerName = input("Wie heißt du? ")
 print("Willkommen", spielerName, "zu BUZZWORD")
-
-#TEST EINER GRID mit Tkinter:   
-def generate_grid(rows, columns):
-    #Erzeugung des Tkinter-Fensters
-    root = tk.Tk()
-    root.title("Buzzword Bingo")
-
-    #Frame für das Raster
-    grid_frame = tk.Frame(root) 
-    grid_frame.pack()
-
-#Erzeugung des Rasters
-    for i in range(rows):
-         for j in range(columns):
-                label = tk.Label(grid_frame, text='', borderwidth=1, relief="solid", width=10, height=5)
-                label.grid(row=i, column=j)
- 
-    root.mainloop()
-
-def main():
-    rows = int(input("Anzahl der Zeilen:"))
-    columns = int(input("Anzahl der Spalten:"))
-    generate_grid(rows, columns)
-
-if __name__ == "__main__":
-    main()
-
-        
 
 #Einlesen der Buzzwords-Datei
 print("Nenne den Namen der Datei:") #C:/Users/covrk/Documents/GitHub/BSRN-Gruppenaufgabe/Buzzwords.txt
@@ -48,5 +21,42 @@ if wort in roudnfileOpener.read():
     roudnfileOpener.close()
 else:
     print("Dieses Buzzword ist leider nicht dabei")
+#TEST EINER GRID mit Tkinter:   
+def generate_grid(rows, columns):
+    #Erzeugung des Tkinter-Fensters
+    root = tk.Tk()
+    root.title("Buzzword Bingo")
+
+    #Frame für das Raster
+    grid_frame = tk.Frame(root) 
+    grid_frame.pack()
+
+#Erzeugung des Rasters
+    for i in range(rows):
+         for j in range(columns):
+                label = tk.Label(grid_frame, text= wort, borderwidth=1, relief="solid", width=10, height=5)
+                label.grid(row=i, column=j)
+ 
+    root.mainloop()
+
+def read_buzzword(roundfile):
+     with open(roundfile, 'r') as f:
+        lines = f.readlines()
+        random_line = random.choice(lines)
+        return random_line
+
+random_line = read_buzzword(roundfile)
+
+def main():
+    rows = int(input("Anzahl der Zeilen:"))
+    columns = int(input("Anzahl der Spalten:"))
+    generate_grid(rows, columns)
+
+if __name__ == "__main__":
+    main()
+
+        
+
+
 
 
