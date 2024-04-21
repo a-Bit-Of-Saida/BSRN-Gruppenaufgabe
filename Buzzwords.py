179eb053aaf92a371b11894dde85ab3d41eb4771
Saida <covrksaida@gmail.com>
2024-04-21 15:02:19 +0200

    Aufräumen
    Code etwas aufgeräumt
    Pynput funktioniert, erster Test ist drin (Zeile. 9-11)
    Dateipfad für das roundfile wurde geändert
    Width und height für die Grid wurden angepasst
    import von argparse, um aus dem Terminal heraus Befehle zu ermöglichen (Implementation fehlt!)

--- Buzzwords.py
+++ Buzzwords.py
@@ -1,29 +1,38 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+import argparse #lässt Befehle aus einem Terminal ausführen
 
-#import pynput für Tastatureingabe funktioniert nicht
+from pynput import keyboard
+from pynput.keyboard import Key
+
+def on_key_release(key):
+    if key == Key.right:
+        print("Rechte Taste gedrückt")
 
 #import textual
 spielerName = input("Wie heißt du? ")
 print("Willkommen", spielerName, "zu BUZZWORD")
 
 #Einlesen der Buzzwords-Datei
-print("Nenne den Namen der Datei:") #C:/Users/covrk/Documents/GitHub/BSRN-Gruppenaufgabe/Buzzwords.txt
+print("Nenne den Namen der Datei:") #C:\Users\covrk\Documents\GitHub\BSRN-Gruppenaufgabe\Buzzwords-Datei.txt
 roundfile = input()
 roudnfileOpener = open(roundfile, 'r') #Variabeln umbenennen!
 
-#Start des Spiels
-print("Willkommen zum Buzzword-Bingo")
-print("Nenne ein Wort:")
-wort = input()
 
-#Überprüfung ob das Wort in der Datei ist
-if wort in roudnfileOpener.read():
-    print("Dein Wort befindet sich in der Datei")
-    roudnfileOpener.close()
-else:
-    print("Dieses Buzzword ist leider nicht dabei")
+def pruefeObWortRichtig():
+    #Start des Spiels
+    print("Willkommen zum Buzzword-Bingo")
+    print("Nenne ein Wort:")
+    wort = input()
+    #Überprüfung ob das Wort in der Datei ist
+    if wort in random.choice(buzzwords):
+        print("Dein Wort befindet sich in der Datei")
+        roudnfileOpener.close()
+    else:
+        print("Dieses Buzzword ist leider nicht dabei")
+
+
 
 #TEST EINER GRID mit Tkinter:   
 def generate_grid(rows, columns, buzzwords):
@@ -36,15 +45,13 @@ def generate_grid(rows, columns, buzzwords):
     grid_frame.pack()
 
 
-
-
 #Erzeugung des Rasters
     for i in range(rows):
          for j in range(columns):
-                label = tk.Label(grid_frame, text= random.choice(buzzwords), borderwidth=1, relief="solid", width=10, height=5)
+                label = tk.Label(grid_frame, text= random.choice(buzzwords), borderwidth=1, relief="solid", width=25, height=15)
                 label.grid(row=i, column=j)
- 
     root.mainloop()
+
 
 def read_buzzword(roundfile):
     with open(roundfile, 'r') as f:
@@ -57,12 +64,8 @@ def main():
     rows = int(input("Anzahl der Zeilen:"))
     columns = int(input("Anzahl der Spalten:"))
     generate_grid(rows, columns, buzzwords)
+    pruefeObWortRichtig()
 
 if __name__ == "__main__":
     main()
-
-        
-
-
-
 
