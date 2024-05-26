4a6c8388eae0b12f5372aec18b66a43ea93af8bf
Saida <covrksaida@gmail.com>
2024-05-26 20:50:27 +0200

    Kommentare hinzugefügt
    Ich habe ein Paar Kommentare hinzugefügt und schonmal eine Log-Datei, welche aber noch nicht vollständig ist.

--- Buzzword-Bingo-Main.py
+++ Buzzword-Bingo-Main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import TermTk as ttk
+import TermTk as ttk #importiert pyTermTk zum visualisieren
 import random
 from TermTk.TTkCore.cfg import TTkCfg
 from TermTk.TTkCore.constant import TTkK
@@ -9,12 +9,11 @@ from TermTk.TTkLayouts import TTkGridLayout, TTkLayout
 from TermTk.TTkWidgets import TTkWidget
 from TermTk.TTkWidgets.button import TTkButton
 from TermTk.TTkWidgets.resizableframe import TTkResizableFrame
-from datetime import datetime
-import logging
+from datetime import datetime #importiert die Zeit
+import logging #importiert eine bib zur Erstellung einer Log-Datei
 
 spielername = input("Nenne deinen Namen:") # Spielername wird eingegeben vom User
-print("Nenne den Pfad der Buzzwords-Datei:") #Beispiel path: C:\Users\covrk\Documents\GitHub\BSRN-Gruppenaufgabe\Buzzwords-Datei.txt
-roundfile = input() 
+roundfile = input("Nenne den Pfad der Buzzwords-Datei:") #Beispiel path: C:\Users\covrk\Documents\GitHub\BSRN-Gruppenaufgabe\Buzzwords-Datei.txt
 roundfileOpener = open(roundfile, 'r')
 # Speicherort der Logdatei
 log_path = input("Nenne den Pfad des Log Dateien Speicherortes:")
@@ -22,6 +21,7 @@ log_path = input("Nenne den Pfad des Log Dateien Speicherortes:")
 zeilen = int(input("Anzahl an Zeilen:"))
 spalten = int(input("Anzahl an Spalten:"))
 
+#Funktion zur Erstellung der Log-Datei
 def create_log_file(player_name, log_directory):
     os.makedirs(log_directory, exist_ok=True)
     # Erstelle den Log-Dateinamen mit dem aktuellen Datum und dem Spielernamen
@@ -38,7 +38,7 @@ def read_buzzword(roundfile):
         lines = f.readlines()
         return lines
 
-buzzwords = read_buzzword(roundfile)
+buzzwords = read_buzzword(roundfile) #die Buzzwords sind die ausgelesenen Wörter der Datei mit dem var Namen roundfile
 
 # Erstelle die Logdatei
 create_log_file(spielername, log_path) # ruft die Log-File Methode auf und übergibt den Spielernamen sowie den Pfad zur Speicherung der Log_Datei
@@ -59,8 +59,8 @@ used_buzzwords = set()
 # Funktion zum Beenden des Spiels
 def spiel_beenden():
     now = datetime.now() # Variable now nimmt den Wert der derzeitigen Zeit an
-    logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Spiel durch den Beenden Button beendet")
-    sys.exit(0)
+    logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Spiel durch den Beenden Button beendet") #in der Log_Datei als Beenden durch Button eingetragen
+    sys.exit(0) #Beendet das Programm 
 
 # Log-Datei Eintrag bei Anklicken eines Buttons
 def log_buzzword(button):
